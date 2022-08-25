32f614c43de803916f9e0e7bf8ce318140aab435
Metone1 <Evgeny.Chernov.One@gmail.com>
2022-08-25 13:50:44 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const mySwiper = new Swiper(
+   '.swiper-container', {
+   direction: 'horizontal',
+   initialSlide: 0,
+   grabCursor: true,
+   pagination: {
+      el: '.swiper-pagination',
+   }
+});

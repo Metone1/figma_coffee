ce1ff87627777f122ec6fb830f2d048616b1857f
Metone1 <Evgeny.Chernov.One@gmail.com>
2022-09-21 22:47:30 +0300

    burger_refactoring

--- js/script.js
+++ js/script.js
@@ -9,10 +9,17 @@ const mySwiper = new Swiper(
 });
 
 
+//       document.body.classList.toggle('lock');
+
 const burgerMenu = document.querySelector('.top__burger');
-if (burgerMenu) {
-   burgerMenu.addEventListener('click', function () {
-      document.body.classList.toggle('lock');
-      burgerMenu.classList.toggle('active');
-   });
-}
+const checkbox = document.querySelector('.burger__checkbox');
+checkbox.addEventListener('click', function () {
+   if (checkbox.checked == true) {
+      burgerMenu.classList.add('active');
+      document.body.classList.add('lock');
+   }
+   else {
+      burgerMenu.classList.remove('active');
+      document.body.classList.remove('lock');
+   }
+});
